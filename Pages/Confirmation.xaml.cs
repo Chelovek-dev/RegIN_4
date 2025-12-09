0b1842df2866a67b991cef47b61ae36a10313b31
Chelovek <forstudkak@gmail.com>
2025-12-10 01:23:42 +0500

    Программирование окна Confirmation. Булатов

--- Pages/Confirmation.xaml.cs
+++ Pages/Confirmation.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RegIN_Bulatov_Perevozshikova.Pages
 {
@@ -20,9 +11,61 @@ namespace RegIN_Bulatov_Perevozshikova.Pages
     /// </summary>
     public partial class Confirmation : Page
     {
-        public Confirmation()
+        public enum TypeConfirmation
+        {
+            Login,
+            Regin
+        }
+        TypeConfirmation ThisTypeConfirmation;
+        public int Code = 0;
+        public Confirmation(TypeConfirmation TypeConfirmation)
         {
             InitializeComponent();
+            ThisTypeConfirmation = TypeConfirmation;
+            SendMailCode();
+        }
+        public void SendMailCode()
+        {
+            Code = new Random().Next(100000, 999999);
+            Classes.SendMail.SendMessage($"Login code: {Code}", MainWindow.mainWindow.UserLogin.Login);
+            TimerSendMailCode();
+        }
+        public async void TimerSendMailCode()
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                LTimer.Content = $"A second message can be sent after {60 - i} seconds";
+                await Task.Delay(1000);
+            }
+
+            BSendMessage.IsEnabled = true;
+            LTimer.Content = "";
+        }
+        private void SendMail(object sender, RoutedEventArgs e) => SendMailCode();
+        private void SetCode(object sender, KeyEventArgs e)
+        {
+            if (TbCode.Text.Length == 6)
+                SetCode();
+
+        }
+        private void SetCode(object sender, RoutedEventArgs e) => SetCode();
+        void SetCode()
+        {
+            if (TbCode.Text == Code.ToString() && TbCode.IsEnabled == true)
+            {
+                TbCode.IsEnabled = false;
+                if (ThisTypeConfirmation == TypeConfirmation.Login)
+                {
+                    MessageBox.Show("Авторизация пользоватлея ");
+                }
+                MainWindow.mainWindow.UserLogin.SetUser();
+                MessageBox.Show("Регистарция пользоватлея ");
+
+            }
+        }
+        private void OpenLogin(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.OpenPage(new Login());
         }
     }
 }
