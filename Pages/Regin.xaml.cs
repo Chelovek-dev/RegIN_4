3636f320a251b4b6fec02cdf78907b4e88b69b10
ff <ddelta482@gmail.com>
2025-12-10 16:30:43 +0500

    шаг 64-66.пермилка

--- Pages/Regin.xaml.cs
+++ Pages/Regin.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 using Microsoft.Win32;
 
 namespace RegIN_Bulatov_Perevozshikova.Pages
@@ -68,6 +70,112 @@ namespace RegIN_Bulatov_Perevozshikova.Pages
             SetNotification("Invalid login", Brushes.Red);
             // Вызываем метод авторизации
             OnRegin();
+        }
+        #region SetPassword
+        /// <summary>
+        /// Метод ввода пароля
+        /// </summary>
+        private void SetPassword(object sender, System.Windows.RoutedEventArgs e) =>
+            SetPassword();
+        private void SetPassword(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                SetPassword();
+        }
+
+        /// <summary>
+        /// Метод ввода пароля
+        /// </summary>
+        public void SetPassword()
+        {
+            Regex regex = new Regex(@"(?=.*[0-9])(?=.*[!@#$%^&?*\-\-=])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&?*\-\-=]{10,}");
+            // (?=.*[0-9]) - строка содержит хотя бы одно число;
+            // (?=.*[!@#$%^&?*\-\-=]) - строка содержит хотя бы один спецсимвол;
+            // (?=.*[a-z]) - строка содержит хотя бы одну латинскую букву в нижнем регистре;
+            // (?=.*[A-Z]) - строка содержит хотя бы одну латинскую букву в верхнем регистре;
+            // [0-9a-zA-Z!@#$%^&?*\-\-=]{10,} - строка состоит не менее, чем из 10 вышеупомянутых символов.
+
+            BCorrectPassword = regex.IsMatch(TbPassword.Password);
+            if (regex.IsMatch(TbPassword.Password) == true)
+            {
+                SetNotification("", Brushes.Black);
+                if (TbConfirmPassword.Password.Length > 0)
+                    ConfirmPassword(true);
+                OnRegin();
+            }
+            else
+                SetNotification("Invalid password", Brushes.Red);
+        }
+        #endregion
+
+        #region SetConfirmPassword
+        /// <summary>
+        /// Метод повторного ввода пароля
+        /// </summary>
+        private void ConfirmPassword(object sender, KeyEventArgs e)
+        {
+            // Если нажата клавиша Enter
+            if (e.key == Key.Enter)
+                // Вызываем метод повторения пароля
+                ConfirmPassword();
+        }
+
+        /// <summary>
+        /// Метод повторного ввода пароля
+        /// </summary>
+        private void ConfirmPassword(object sender, System.Windows.RoutedEventArgs e) =>
+            // Вызываем метод повторения пароля
+            ConfirmPassword();
+
+        /// <summary>
+        /// Метод повторного ввода пароля
+        /// </summary>
+        public void ConfirmPassword(bool Pass = false)
+        {
+            // Записываем результат сравнения паролей в переменную
+            BCorrectConfirmPassword = TbConfirmPassword.Password == TbPassword.Password;
+            // Если пароль не совпадает с повторением пароля
+            if (TbConfirmPassword.Password != TbPassword.Password)
+                // Выводим сообщение о том, что пароли не совпадают, красным цветом
+                SetNotification("Passwords do not match", Brushes.Red);
+            else
+            {
+                // Если пароли совпадают, выводим пустое сообщение чёрным цветом
+                SetNotification("", Brushes.Black);
+                // Если проверка идёт не из метода проверки пароля
+                // Исключаем зацикливание методов
+                if (!Pass)
+                    // Вызываем проверку пароля
+                    SetPassword();
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Метод регистрации
+        /// </summary>
+        void OnRegin()
+        {
+            if (!BCorrectLogin)
+                return;
+            if (TbName.Text.Length == 0)
+                return;
+            if (!BCorrectPassword)
+                return;
+            if (!BCorrectConfirmPassword)
+                return;
+            MainWindow.mainWindow.UserLogIn.Login = TbLogin.Text;
+            MainWindow.mainWindow.UserLogIn.Password = TbPassword.Password;
+            MainWindow.mainWindow.UserLogIn.Name = TbName.Text;
+            if (BSetImages)
+                MainWindow.mainWindow.UserLogIn.Image = File.ReadAllBytes(Directory.GetCurrentDirectory() + @"\IUser.jpg");
+            MainWindow.mainWindow.UserLogIn.DateUpdate = DateTime.Now;
+            MainWindow.mainWindow.UserLogIn.DateCreate = DateTime.Now;
+            MainWindow.mainWindow.OpenPage(new Confirmation(Confirmation.TypeConfirmation.Regin));
+        }
+        private void SetName(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !(Char.IsLetter(e.Text, 0));
         }
     }
 }
